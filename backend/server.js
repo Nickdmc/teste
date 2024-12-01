be895939a9fe8891c5ed4471318cff84e1755a82
Nicolas <danielnicolas930@gmail.com>
2024-12-01 20:32:00 -0300

    commit 2

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 // Disable dotenv in a production environment
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config(); // Carrega as variáveis do arquivo .env
+  require('dotenv').config(); 
 }
 
 const express = require('express');
@@ -8,33 +8,33 @@ const cors = require('cors');
 const logger = require('morgan');
 const helmet = require('helmet');
 
-// Importa as rotas
+
 const routes = require('./routes');
 
-// Inicializa o express
+
 const app = express();
 
-// Define a porta, com fallback para 3335
+
 const port = process.env.PORT || 3335;
 
-// Configuração do CORS
+
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || '*', // Permite acesso do frontend, usa "*" se não configurado
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
-  allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+  origin: process.env.FRONTEND_URL || '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
 };
 
-// Middlewares
-app.use(cors(corsOptions)); // Habilita CORS
-app.use(express.json()); // Suporte a JSON no corpo das requisições
-app.use(express.urlencoded({ extended: true })); // Suporte para dados de formulários
-app.use(logger('dev')); // Loga requisições em desenvolvimento
-app.use(helmet()); // Segurança adicional para headers HTTP
 
-// Middleware para rotas
-app.use('/api', routes); // Prefixa as rotas com "/api"
+app.use(cors(corsOptions)); 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+app.use(logger('dev')); 
+app.use(helmet()); 
 
-// Listener para inicializar o servidor
+
+app.use('/api', routes); 
+
+
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
